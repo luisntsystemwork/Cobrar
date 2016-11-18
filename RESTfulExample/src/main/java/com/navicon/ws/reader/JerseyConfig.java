c1fd18d07595a5e1e8592314b0c80be6a0ccfb26
Luis Moyano <luis_moyano@intranet.itresources>
2016-11-18 11:15:54 -0300

    property externalizado

--- RESTfulExample/src/main/java/com/navicon/ws/reader/JerseyConfig.java
+++ RESTfulExample/src/main/java/com/navicon/ws/reader/JerseyConfig.java
@@ -1,5 +1,8 @@
 package com.navicon.ws.reader;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -11,14 +14,26 @@ public class JerseyConfig extends Application {
 	public static final String PROPERTIES_FILE = "config.properties";
 	public static Properties properties = new Properties();
 	static {
-		InputStream inputStream = JerseyConfig.class.getClassLoader().getResourceAsStream(PROPERTIES_FILE);
-		if (inputStream != null) {
-			try {
+		/*InputStream inputStream = JerseyConfig.class.getClassLoader().getResourceAsStream(PROPERTIES_FILE);*/
+		String path = File.separator + "ServidorOXP";
+		if (System.getProperty("user.dir").indexOf("ServidorOXP") != -1) {
+
+            path = "C:\\ServidorOXP";        
+		}
+		
+		try {
+			InputStream inputStream = new FileInputStream(new File(path + File.separator + PROPERTIES_FILE));
+		
+			if (inputStream != null) {
 				properties.load(inputStream);
-			} catch (IOException e) {
-				// TODO Add your custom fail-over code here
-				e.printStackTrace();
 			}
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		 catch (IOException e) {
+			// TODO Add your custom fail-over code here
+			e.printStackTrace();
 		}
 	}
 
